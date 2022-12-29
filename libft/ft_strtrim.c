0db10bbddd2ccb6c8c1e98663945a714223d3ce1
Arvind Dhaka <adhaka@2-H-5.42heilbronn.de>
2022-12-29 05:34:26 +0100

    done

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,11 +6,12 @@
 /*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 20:42:02 by adhaka            #+#    #+#             */
-/*   Updated: 2022/12/29 03:44:42 by adhaka           ###   ########.fr       */
+/*   Updated: 2022/12/29 05:14:21 by adhaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -18,8 +19,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*new;
 	size_t	len;
 
-	if (!s1 || !set)
-		return (NULL);
+	if (s1[0] == '\0')
+		return (ft_strdup(""));
 	while (*s1 && ft_strchr(set, *s1))
 			s1++;
 	len = ft_strlen(s1);
